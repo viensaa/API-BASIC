07d1d01b92c670c69a253974dcc346fa5ee815d2
rizal <rizal@DESKTOP-23RBQ8Q>
2022-07-14 13:31:09 +0700

    membuat interface

--- SampleWebAPI.Data/DAL/ICrud.cs
+++ SampleWebAPI.Data/DAL/ICrud.cs
@@ -8,5 +8,10 @@ namespace SampleWebAPI.Data.DAL
 {
     public interface ICrud<T>
     {
+        Task<IEnumerable<T>> GetAll();
+        Task<T>GetById(int id);
+        Task<T> Insert(T obj);
+        Task<T> Update(T obj);
+        Task DeleteById(int id);
     }
 }
