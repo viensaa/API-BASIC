7d93ee94d07681d2b8cbf91d0b629f2f62e5c374
rizal <rizal@DESKTOP-23RBQ8Q>
2022-07-14 13:33:00 +0700

    implementasi interface pada SamuraiDAL

---
+++ SampleWebAPI.Data/DAL/SamuraiDAL.cs
@@ -0,0 +1,42 @@
+﻿using SampleWebAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Data.DAL
+{
+    public class SamuraiDAL : ISamurai
+    {
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Samurai>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Samurai> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Samurai>> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Samurai> Insert(Samurai obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Samurai> Update(Samurai obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
